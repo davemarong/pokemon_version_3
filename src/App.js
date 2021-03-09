224fa1848b4a643ac30656d7899330aa80cc98f4
Dave Marong <kmarong@hotmail.com>
2021-03-09 13:35:48 +0100

    Create guessing game with pokemon data in js
    - In the first part of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GeussingField from "./GeussingField";
+import Header from "./Header";
 
-function App() {
+export default function App() {
+  const pokemonArray = [
+    {
+      Type: {
+        Type1: "Dark",
+        Type2: "Rock",
+      },
+      Pokedex: "#248",
+      Legendary: "No",
+      EvStages: "3",
+      FirstStage: "Larvitar",
+      SecondStage: "Pupitar",
+      Colors: {
+        Primary: "Green",
+        Secondary: "Blue/Black",
+      },
+      Generation: "2",
+      Category: "Armor pokemon",
+      Name: "Tyranitar",
+      Src:
+        "https://cdn.bulbagarden.net/upload/thumb/8/82/248Tyranitar.png/1200px-248Tyranitar.png",
+    },
+    {
+      Type: {
+        Type1: "Water",
+        Type2: "Flying",
+      },
+      Pokedex: "#130",
+      Legendary: "No",
+      EvStages: "2",
+      FirstStage: "Magikarp",
+      Colors: {
+        Primary: "Blue",
+        Secondary: "Tan/White",
+      },
+      Generation: "1",
+      Category: "Atrocious pokemon",
+      Name: "Gyrados",
+      Src: "https://cdn.bulbagarden.net/upload/4/41/130Gyarados.png",
+    },
+    {
+      Type: {
+        Type1: "Psychic",
+        Type2: "Psychic",
+      },
+      Pokedex: "#064",
+      Legendary: "No",
+      EvStages: "3",
+
+      FirstStage: "Abra",
+      ThirdStage: "Alakazam",
+      Colors: {
+        Primary: "Yellow",
+        Secondary: "Brown",
+      },
+      Generation: "1",
+      Category: "Psi pokemon",
+      Name: "Kadabra",
+      Src: "https://cdn.bulbagarden.net/upload/9/97/064Kadabra.png",
+    },
+    {
+      Type: {
+        Type1: "Fire",
+        Type2: "Bug",
+      },
+      Pokedex: "#637",
+      Legendary: "No",
+      EvStages: "2",
+
+      FirstStage: "Larvesta",
+      Colors: {
+        Primary: "Red",
+        Secondary: "Black/White",
+      },
+      Generation: "5",
+      Category: "Sun pokemon",
+      Name: "Volcarona",
+      Src: "https://cdn.bulbagarden.net/upload/6/6b/637Volcarona.png",
+    },
+    {
+      Type: {
+        Type1: "Flying",
+        Type2: "Psychic",
+      },
+      Pokedex: "#249",
+      Legendary: "Yes",
+      EvStages: "1",
+
+      Colors: {
+        Primary: "Silver",
+        Secondary: "Lightblue/blue",
+      },
+      Generation: "2",
+      Category: "Diving pokemon",
+      Name: "Lugia",
+      Src:
+        "https://cdn.bulbagarden.net/upload/thumb/4/44/249Lugia.png/1200px-249Lugia.png",
+    },
+    {
+      Type: {
+        Type1: "Ghost",
+        Type2: "Fire",
+      },
+      Pokedex: "#609",
+      Legendary: "No",
+      EvStages: "3",
+
+      FirstStage: "Litwick",
+      SecondStage: "Lampent",
+      Colors: {
+        Primary: "Purple",
+        Secondary: "Black",
+      },
+      Generation: "5",
+      Category: "Luring pokemon",
+      Name: "Chandelure",
+      Src: "https://cdn.bulbagarden.net/upload/6/65/609Chandelure.png",
+    },
+  ];
+  let hintList = {
+    name: "...",
+    type1: "...",
+    type2: "...",
+    id: "...",
+    generation: "...",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header pokemonArray={pokemonArray} />
+      <GeussingField hintList={hintList} pokemonArray={pokemonArray} />
     </div>
   );
 }
-
-export default App;
