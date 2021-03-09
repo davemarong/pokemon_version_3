1a799ecccd8f5b65ad559362869f44098eff93c1
Dave Marong <kmarong@hotmail.com>
2021-03-09 06:37:19 +0100

    Revert "Create fetch pokemon function"
    This reverts commit 5c43c0375849f4569315cb9dc70267b5517c333c.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import React from "react";
-import Header from "./Header";
-import FetchPokemon from "./FetchPokemon";
-import DisplayPokemon from "./DisplayPokemon";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-  let pokemonList = [
-    // {
-    //   id: "",
-    //   name: "",
-    //   type1: "",
-    //   type2: "",
-    //   order: "",
-    // },
-  ];
-
-  function fetch() {
-    const randomNumber = Math.floor(Math.random() * 151) + 1;
-    const url = `https://pokeapi.co/api/v2/pokemon/2`;
-    console.log(url);
-    const pokemon = fetch(url).then((res) => {
-      res.json().then((result) => {
-        return result;
-      });
-    });
-
-    console.log(pokemon);
-  }
+function App() {
   return (
-    <>
-      <Header />
-      <FetchPokemon pokemonList={pokemonList} fetch={fetch} />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
